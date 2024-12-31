f7c359deb3e03ba16471930080e3905a91a37a50
chouxiaoe <52995333+chouxiaoe@users.noreply.github.com>
2024-12-31 12:56:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,13 +13,19 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-
+sed -i "s/OpenWrt /JiaoYa build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 管理地址
-sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.1.199.1/g' package/base-files/files/bin/config_generate
 
 # 主机名
-sed -i 's/OpenWrt/OpenRoute/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/OpenRoute/g' package/base-files/files/bin/config_generate
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+sed -i 's/luci-theme-bootstrap/luci-theme-opentopd/g' feeds/luci/collections/luci/Makefile
+
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 默认主题
 rm -rf feeds/kenzo/luci-theme-argon
